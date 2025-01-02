c0e7360c6b24c5b0a624ae4e329a303e82f97fc8
KomalSinghMarkam <157139840+KomalSinghMarkam@users.noreply.github.com>
2025-01-02 12:35:57 +0000

    operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,5 @@
+
+//******************** CONVERSION***************
 let score =true
 console.log(typeof score);
 
@@ -16,3 +18,43 @@ console.log(booleanIsLoggedIn);
 // 1 => true; 0 => false
 // "" => false for empty String
 // "komal" => true
+let num = 33
+let str = String(num)
+console.log(str);
+console.log(typeof str);
+
+
+//********************OPERATIONS**************** 
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "komal"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 3);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 +4 ) * 5 % 3);
+console.log(+true);
+console.log(+"");
+
+
+
+
+
