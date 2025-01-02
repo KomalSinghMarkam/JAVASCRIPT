507bd4fda01bcba02230e4114a6bb33e66752c1c
KomalSinghMarkam <157139840+KomalSinghMarkam@users.noreply.github.com>
2025-01-02 12:13:12 +0000

    onversionofdatatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+let score =true
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// when you try to convert number "33" => 33
+// "33abs" =>NaN
+// true => 1;  false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false for empty String
+// "komal" => true
