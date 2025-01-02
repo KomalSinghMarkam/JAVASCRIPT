b2159e4f4904775c68c28149fe1a2bb5578d7f8e
KomalSinghMarkam <157139840+KomalSinghMarkam@users.noreply.github.com>
2025-01-02 11:34:20 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs,not browser
+
+console.log(3+3);
+
+
+let name = "komal"
+let age = 21
+let isLoggedIn = false
+let state; //undefined variable
+
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
