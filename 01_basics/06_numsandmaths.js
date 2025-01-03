9a74657fb0a86401edfe5fe2be382deb7442b81c
KomalSinghMarkam <157139840+KomalSinghMarkam@users.noreply.github.com>
2025-01-03 19:20:07 +0000

    nums and maths

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,36 @@
+const score = 89
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+console.log(balance.toString().length);
+
+const othernumber = 2389.899
+
+console.log(othernumber.toPrecision(4));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,6,8,2,6,83,3,3,54));
+// console.log(Math.max(4,3,6,4,5,3,3,3,4,4,444,));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)))
+
